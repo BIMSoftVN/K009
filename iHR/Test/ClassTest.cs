eebf1f468468d35575e602dc85fe948c244f3fa7
Thế Anh Đỗ <theanhdo@live.com>
2025-02-13 22:06:14 +0700

    Bài 3 : Lam quen voi C#

--- iHR/Test/ClassTest.cs
+++ iHR/Test/ClassTest.cs
@@ -1,12 +1,30 @@
-﻿using System;
+﻿using NUnit.Framework;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
 namespace iHR.Test
 {
-    internal class ClassTest
+    [TestFixture]
+    public class ClassTest
     {
+        [Test]
+        public void TestMethod()
+        {
+            //string Name = "Phong Vũ 2";
+            //int i = 10;
+            //int j = 20;
+            //int k = i + j;
+
+            int SoNguyen = 20;
+            double SoThuc = SoNguyen;
+            int SoNguyen2 = SoThuc;
+
+
+            Console.WriteLine(SoNguyen2);
+        }
     }
 }
