d892fe909cbc5c7708a9e820dd120b7d69e814bb
Thế Anh Đỗ <theanhdo@live.com>
2025-02-25 07:55:53 +0700

    Bài 6

--- iHRM/View/vSignIn.xaml.cs
+++ iHRM/View/vSignIn.xaml.cs
@@ -10,17 +10,16 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace iHRM
+namespace iHRM.View
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for vSignIn.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class vSignIn : Window
     {
-        public MainWindow()
+        public vSignIn()
         {
             InitializeComponent();
         }
