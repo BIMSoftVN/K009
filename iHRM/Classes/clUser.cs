2b850066199aa6f2967968f1bdecb39be43d8525
Thế Anh Đỗ <theanhdo@live.com>
2025-03-17 22:19:10 +0700

    Bài 12

---
+++ iHRM/Classes/clUser.cs
@@ -0,0 +1,83 @@
+﻿using K009Libs.MVVM;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace iHRM.Classes
+{
+    public class clUser : PropertyChangedBase
+    {
+        private int? _Id = null;
+        public int? Id
+        {
+            get
+            {
+                return _Id;
+            }
+            set
+            {
+                _Id = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private string _Name = null;
+        public string Name
+        {
+            get
+            {
+                return _Name;
+            }
+            set
+            {
+                _Name = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private string _PhoneNumber = null;
+        public string PhoneNumber
+        {
+            get
+            {
+                return _PhoneNumber;
+            }
+            set
+            {
+                _PhoneNumber = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private DateTime? _DateOfBirth = null;
+        public DateTime? DateOfBirth
+        {
+            get
+            {
+                return _DateOfBirth;
+            }
+            set
+            {
+                _DateOfBirth = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private string _Address = null;
+        public string Address
+        {
+            get
+            {
+                return _Address;
+            }
+            set
+            {
+                _Address = value;
+                OnPropertyChanged();
+            }
+        }
+
+    }
+}
---
+++ iHRM/Model/mUser.cs
@@ -0,0 +1,54 @@
+﻿using iHRM.Classes;
+using Newtonsoft.Json.Linq;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SQLite;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace iHRM.Model
+{
+    [TestFixture]
+    public class mUser
+    {
+        [Test]
+        public async Task Test()
+        {
+            await GetAllUsers(@"C:\Users\kysudo\Desktop\K009.db");
+        }
+
+
+        public static async Task<List<clUser>> GetAllUsers(string SqlFilePath)
+        {
+            var uList = new List<clUser>();
+
+            string ConnString = @"Data Source=" + SqlFilePath + ";Version=3;";
+
+
+            using (var conn = new SQLiteConnection(ConnString))
+            {
+                conn.Open();
+
+                using (var cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = "SELECT * FROM [Users]";
+                    var reader = await cmd.ExecuteReaderAsync();
+                    var dt = new DataTable();
+                    dt.Load(reader);
+
+                    if (dt!=null && dt.Rows.Count>0)
+                    {
+                        uList = JToken.FromObject(dt).ToObject<List<clUser>>();
+                    }    
+                }
+
+                Console.WriteLine(SqlFilePath);
+            }
+
+            return uList;
+        }
+    }
+}
